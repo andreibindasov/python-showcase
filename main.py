ea5e1e7acf3091ff2b36e583d7a17d7739350b77
andreibindasov <bindasov.andrei@gmail.com>
2023-06-06 14:55:51 -0600

    Add description on the new columns

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import streamlit as st
 
 # st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+with st.container():
+    col1, col2 = st.columns(2)
 
-with col1:
-    st.image("images/photo.jpg")
+    with col1:
+        st.image("images/photo.jpg")
 
-with col2:
-    st.title("Andrei Bindasov")
-    content = """
-    Hi, I am a man: Andrei! I am a programmer, linguist, coach and a teacher. I graduated in 1996 with 
-    MD in Computer Science & BD in Linguistics.
-    """
-    st.write(content)
+    with col2:
+        st.title("Andrei Bindasov")
+        content = """
+        Hi, I am a man: Andrei! I am a programmer, linguist, coach and a teacher. I graduated in 1996 with 
+        MD in Computer Science & BD in Linguistics.
+        """
+        st.write(content)
+
+st.write("Below you can find python showcase projects. Feel free to contact me.")
+

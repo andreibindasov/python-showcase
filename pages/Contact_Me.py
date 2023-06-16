b10b0e87b61527177ffdd3ee60738f74853d5965
andreibindasov <bindasov.andrei@gmail.com>
2023-06-16 13:46:20 -0600

    send email implemented + env variables stored

--- pages/Contact_Me.py
+++ pages/Contact_Me.py
@@ -1,10 +1,16 @@
 import streamlit as st
+from send_email import send_email
 
 st.header("Contact Me")
 
 with st.form(key="contact_form"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+
+    message = f"Subject:New email from {user_email} From:{user_email}\n{raw_message}"
+
     button = st.form_submit_button()
     if button:
-        pass
+        send_email(message)
+        st.info("Your email was successfully sent")
+
--- send_email.py
+++ send_email.py
@@ -1,25 +1,19 @@
 import smtplib
 import ssl
+import os
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "andrei.bindasov73@gmail.com"
-phrase = "nfwwqyfewygkgzzb"
+def send_email(user_message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = os.getenv("PYTHON_USER_EMAIL")
+    phrase = os.getenv("PASSPHRASE")
 
-receiver = ["abraxas777ainsoph@protonmail.com", "bindasov.andrei@gmail.com"]
-# receiver = "bindasov.andrei@gmail.com"
-
-context = ssl.create_default_context()
-
-subject = "URGENT!!! HELP!!!"
-message = """
-Hi!
-How are you?
-Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, phrase)
-    server.sendmail(username, receiver, f"Subject:{subject}\n{message}")
-
+    receiver = ["abraxas777ainsoph@protonmail.com", "bindasov.andrei@gmail.com"]
+    # receiver = "bindasov.andrei@gmail.com"
+    context = ssl.create_default_context()
+    # subject = user_email
+    message = user_message
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, phrase)
+        server.sendmail(username, receiver, message)
